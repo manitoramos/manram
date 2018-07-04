97fb30ee89bd91b9caa81b1bc55595a4fdaa4819
Unknown <mtjmt2@hotmail.com>
2018-07-04 12:12:24 +0100

    alteração para conseguir entrar num localhost com pass

--- assets/bd/bd.php
+++ assets/bd/bd.php
@@ -4,10 +4,14 @@
 		$BD = mysqli_connect("localhost", "root", "", "manitoramos");
 		
 		if (!$BD) {
-			echo "Error: Unable to connect to MySQL." . PHP_EOL;
-			echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
-			echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
-			exit;
+			$BD = mysqli_connect("localhost", "root", "Mobinteg123!..", "manitoramos");
+			
+			if(!$BD){
+				echo "Error: Unable to connect to MySQL." . PHP_EOL;
+				echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+				echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+				exit;
+			}
 		}
 
 		/*echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
